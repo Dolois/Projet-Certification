3f46c4a87221d693720e0b1b87c0734a34bc736a
Dolois <dloisel@sfr.fr>
2018-11-10 04:37:05 +0100

    creation de mon readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+### Certification Api / Rest / Spring
+
+[Objectif] (Certification.pptx) 
+
+[Trello] (https://trello.com/b/DrJ6bvKT/certification)
---
+++ src/main/java/co/simplon/certification/CertificationApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationApplication.class, args);
+	}
+}

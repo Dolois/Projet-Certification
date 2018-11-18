e9f6ca2e6d5309b4028c20b24163b5bba9f4078c
Dolois <dloisel@sfr.fr>
2018-11-18 17:38:41 +0100

    scripts BDD sportlib

---
+++ src/main/resources/insert_table_activite.sql
@@ -0,0 +1,77 @@
+USE `sportlib`;
+
+INSERT INTO `activite` (etablissement, nom, adresse, ville, codepostal, telephone, jour_1, jour_2, jour_3, jour_4, jour_5, jour_6, jour_7, image, ref_discipline)
+VALUES (
+'Stade',
+'Nautique de Pessac',
+'13 Avenue des Aciéries',
+'Pessac',
+'33600',
+'05 56 07 35 84',
+'Lundi    12h00 à 14h00 et de 17h00 à 19h30',
+'Mardi    12h00 à 14h00 et de 17h00 à 21h00',
+'Mercredi 10H00 à 19h30',
+'Jeudi    12h00 à 14h00 et de 17h00 à 21h00',
+'Vendredi 12h00 à 14h00 et de 17h00 à 19h30',
+'Samedi   11h00 à 19h00',
+'Dimanche 10H00 à 19h00',
+'https://www.bing.com/th?id=A%2boclkcL9kFxWEQ480x360&pid=Local',
+1
+);
+
+INSERT INTO `activite` (etablissement, nom, adresse, ville, codepostal, telephone, jour_1, jour_2, jour_3, jour_4, jour_5, jour_6, jour_7, image, ref_discipline)
+VALUES (
+'Piscine',
+'Georges Vallerey',
+'140 Avenue Gambetta',
+'Paris',
+'75020',
+'01 40 31 15 20',
+'Lundi    11h45 à 13h30',
+'Mardi    11h45 à 13h30 et de 17h15 à 22h00',
+'Mercredi 07H00 à 08h30 et de 12h30 à 19h00',
+'Jeudi    11h45 à 13h30 et de 17h15 à 22h00',
+'Vendredi 07h00 à 18h00',
+'Samedi   11h00 à 19h00',
+'Dimanche 11H00 à 19h00',
+'https://www.guide-piscine.fr/medias/image/piscine_georges_vallerey_a_paris_20e_-2543-300-0.jpg',
+1
+);
+
+INSERT INTO `activite` (etablissement, nom, adresse, ville, codepostal, telephone, jour_1, jour_2, jour_3, jour_4, jour_5, jour_6, jour_7, image, ref_discipline)
+VALUES (
+'Stade',
+'InfraStructure sports et loisirs',
+'30 rue Louis Lumière',
+'Paris',
+'75020',
+'01 43 70 86 32',
+'Lundi    18h00 à 22h00',
+'Mardi    18h00 à 22h00',
+'Mercredi 18h30 à 22h00',
+'Jeudi    Fermeture',
+'Vendredi 19h30 à 22h00',
+'Samedi   Fermeture',
+'Dimanche Fermeture',
+'http://www.ethic-etapes.com/images/upload/portfolio_img/rip_301275987391.jpg',
+2
+);
+
+INSERT INTO `activite` (etablissement, nom, adresse, ville, codepostal, telephone, jour_1, jour_2, jour_3, jour_4, jour_5, jour_6, jour_7, image, ref_discipline)
+VALUES (
+'Salle',
+'Club de Gymnastique',
+'39 rue des Amandiers',
+'Paris',
+'75020',
+'06 83 46 68 96',
+'Lundi    11h00 à 21h00',
+'Mardi    11h00 à 21h00',
+'Mercredi 11h00 à 21h00',
+'Jeudi    11h00 à 21h00',
+'Vendredi 11h00 à 21h00',
+'Samedi   11h00 à 21h00',
+'Dimanche Fermeture',
+'https://s3-eu-west-1.amazonaws.com/g4l-images/551ac363e4b0c9516e4b4dc6/2999b36ff6562a6f0782213232feb6fc',
+2
+);

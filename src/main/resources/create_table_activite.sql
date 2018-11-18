c2bd388cc24fb9ad83e40b5e0213c65edb82b713
LOISEL Dominick <39958651+Dolois@users.noreply.github.com>
2018-11-18 18:10:44 +0100

    Update create_table_activite.sql

--- src/main/resources/create_table_activite.sql
+++ src/main/resources/create_table_activite.sql
@@ -1,21 +1,21 @@
 USE `sportlib`;
 
 CREATE TABLE `activite` (
-	id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     etablissement VARCHAR(80),
     nom VARCHAR(100),
     adresse VARCHAR(80),
     ville VARCHAR(80),
     codepostal VARCHAR(5),
     telephone VARCHAR(20),
-	jour_1 VARCHAR(60),
+    jour_1 VARCHAR(60),
     jour_2 VARCHAR(60),
     jour_3 VARCHAR(60),
     jour_4 VARCHAR(60),
     jour_5 VARCHAR(60),
     jour_6 VARCHAR(60),
     jour_7 VARCHAR(60),
-	image VARCHAR(255),
+    image VARCHAR(255),
     ref_discipline INT,
     PRIMARY KEY ( id )
 );
